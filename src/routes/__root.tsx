2e3cae96f1e5674555bce0a5b0439a774c1a081f
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-08-07 15:58:08 -0300

    refactor: update layout in __root.tsx to use flexbox for improved structure

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-12">
+      <div className="p-12 h-full flex flex-col">
         <Outlet />
       </div>
       <TanStackRouterDevtools />
