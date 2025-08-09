91d0e9a8f2c0306eb4f119683a9401f595ab1956
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-08-09 15:12:33 -0300

    feat: implement video URL validation and error handling in Download component
    - Integrated useMutation from React Query to handle video URL validation.
    - Added global dialog notifications for invalid URLs and error scenarios.
    - Updated InputUrl component to utilize mutation for form submission and loading state management.

--- src/routes/videos/download/index.tsx
+++ src/routes/videos/download/index.tsx
@@ -7,6 +7,9 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { invoke } from '@tauri-apps/api/core';
+import { useGlobalDialogStore } from '@/stores/global-dialog-store';
+import { useMutation } from '@tanstack/react-query';
+
 export const Route = createFileRoute('/videos/download/')({
   component: Download,
 });
@@ -15,10 +18,35 @@ export const InputUrlformSchema = z.object({
   videoUrl: z.url(),
 });
 
-function InputUrlOnSubmit(values: z.infer<typeof InputUrlformSchema>) {
-}
-
 function Download() {
+  const openDialog = useGlobalDialogStore((s) => s.open);
+
+  const mutation = useMutation({
+    mutationFn: async (values: z.infer<typeof InputUrlformSchema>) => {
+      const isValidUrl = await invoke<boolean>('validate_video_url', {
+        url: values.videoUrl,
+      });
+
+      if (!isValidUrl) {
+        await openDialog({
+          title: 'URL inválida',
+          description: 'A URL do vídeo não é válida.',
+          type: 'error',
+        });
+        return;
+      }
+      console.log('URL válida:', values.videoUrl);
+    },
+    onError: async () => {
+      await openDialog({
+        title: 'Erro',
+        description:
+          'Ocorreu um erro ao validar a URL. Tente novamente mais tarde.',
+        type: 'error',
+      });
+    },
+  });
+
   const InputUrlForm = useForm<z.infer<typeof InputUrlformSchema>>({
     resolver: zodResolver(InputUrlformSchema),
     defaultValues: {
@@ -29,7 +57,11 @@ function Download() {
   return (
     <div className="flex h-full space-x-4">
       <div className="flex flex-1 flex-col space-y-4">
-        <InputUrl form={InputUrlForm} onSubmit={InputUrlOnSubmit} />
+        <InputUrl
+          form={InputUrlForm}
+          onSubmit={mutation.mutate}
+          isLoading={mutation.isPending}
+        />
         <VideoPreview />
         <VideoDownloadSettings />
       </div>
